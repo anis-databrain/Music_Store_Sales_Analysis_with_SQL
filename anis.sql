35442e91129ac2e341dcb3e27f6ac7e8ad71ef55
Anisur Rahman <anis.databrain@gmail.com>
2023-12-19 11:19:16 +0600

    Add files via upload

---
+++ anis.sql
@@ -0,0 +1,12 @@
+WITH best_selling_artist AS (
+	SELECT artist.artist_id AS artist_id, artist.name AS artist_name,
+	SUM(invoice_line.unit_price*invoice_line.quantity) AS total_sales
+	FROM invoice_line
+	JOIN track ON track.track_id = invoice_line.track_id
+	JOIN album ON album.album_id = track.album_id
+	JOIN artist ON artist.artist_id = album.artist_id
+	GROUP BY 1
+	ORDER BY 3 DESC
+	LIMIT 1
+	
+)
